e6ff8d18a67fef4807bc884c88b015c29e1be5ca
Mavluda <rahimqulimavluda@gmail.com>
2023-11-01 15:49:17 +0500

    Differensec between ArrayList and LinkedList

---
+++ src/Collections/Differences.java
@@ -0,0 +1,20 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Differences {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        for(int i = 0; i < 10000; i++){
+            arrayList.add(i);
+            linkedList.add(i);
+        }
+
+        long start = System.currentTimeMillis();
+
+
+    }
+}
